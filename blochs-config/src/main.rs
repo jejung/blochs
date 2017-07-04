5699bce6cff498adabfc7530eef28c5a32996dd7
Jean Jung <jean.jung@rocketmail.com>
2017-07-04 01:02:30 -0300

    refactor: move reusable things to libblochs

--- blochs-config/src/main.rs
+++ blochs-config/src/main.rs
@@ -1,34 +1,22 @@
-extern crate toml;
 extern crate clap;
 extern crate libblochs;
 
 use clap::{App, Arg, ArgMatches};
-use std::path::Path;
-use std::fs::{create_dir, File};
-use std::io::prelude::*;
-use std::io::Result;
-use libblochs::config::ServerConfig;
+use libblochs::config::{ServerConfig, load_server_config, store_server_config};
+use libblochs::CONFIG_PATH;
 
-const CONFIG_PATH: &'static str = "/etc/blochs/";
-const CONFIG_FILE_NAME: &'static str = "server.toml";
 const DEFAULT_DATA_DIR: &'static str = "/var/lib/blochs/";
 
 fn main() {
     let options = get_provided_options();
 
-    let config_path = Path::new(CONFIG_PATH);
-    ensure_dir_exists(&config_path);
+    let mut config_values = load_server_config();
 
-    let config_file_path = config_path.join(CONFIG_FILE_NAME);
-    let actual_content = get_file_content(&config_file_path);
-
-    let mut config_values: ServerConfig = toml::from_str(&actual_content).unwrap();
     set_options(&mut config_values, &options);
 
-    let new_config_content = toml::to_string(&config_values).unwrap();
-    match rewrite_file(&config_file_path, &new_config_content) {
-        Ok(_) => println!("New config saved at {}:\n\n{}", config_file_path.display(), new_config_content),
-        Err(why) => panic!("Could not write config file {:?}: {}", config_file_path.display(), why),
+    match store_server_config(&config_values) {
+        Ok(content) => println!("New config saved at {}:\n\n{}", CONFIG_PATH, content),
+        Err(why) => panic!("Could not write config file {:?}: {}", CONFIG_PATH, why),
     };
 }
 
@@ -39,40 +27,6 @@ fn set_options(config_values: &mut ServerConfig, options: &ArgMatches) {
     let new_data_dir = options.value_of("data.dir").unwrap_or(&config_or_default_data_dir);
 
     config_values.data_dir = Some(new_data_dir.to_string());
-}
-
-fn rewrite_file(path: &Path, new_content: &String) -> Result<()> {
-    let mut file = match File::create(path) {
-        Ok(file) => file,
-        Err(err) => panic!("Could not open config file {:?}: {}", path.display(), err),
-    };
-
-    return file.write_all(new_content.as_bytes());
-}
-
-fn get_file_content(path: &Path) -> String {
-    let mut content = String::new();
-    if path.exists() {
-        let mut file = match File::open(&path) {
-            Ok(file) => file,
-            Err(err) => panic!("Could not open file {:?}: {}", path.display(), err),
-        };
-
-        match file.read_to_string(&mut content) {
-            Ok(_) => {},
-            Err(err) => panic!("Could not read file {:?}: {}", path.display(), err),
-        };
-    }
-    return content;
-}
-
-fn ensure_dir_exists(path: &Path) {
-    if !path.exists() {
-        match create_dir(path) {
-            Err(err) => panic!("Could not create directory under {}: {}", path.display(), err),
-            Ok(_) => {}
-        };
-    }
 }
 
 fn get_provided_options<'a>() -> ArgMatches<'a> {
--- libblochs/src/lib.rs
+++ libblochs/src/lib.rs
@@ -2,10 +2,77 @@
 extern crate serde_derive;
 extern crate serde;
 
+pub const CONFIG_PATH: &'static str = "/etc/blochs/server.toml";
+
+pub mod io {
+    use std::path::Path;
+    use std::fs::{create_dir, File};
+    use std::io::Error;
+    use std::io::prelude::*;
+
+    pub fn ensure_dir_exists(path: &Path) {
+        if !path.exists() {
+            match create_dir(path) {
+                Err(err) => panic!("Could not create directory under {}: {}", path.display(), err),
+                Ok(_) => {}
+            };
+        }
+    }
+
+    pub fn get_file_content(path: &Path) -> String {
+        let mut content = String::new();
+        if path.exists() {
+            let mut file = match File::open(&path) {
+                Ok(file) => file,
+                Err(err) => panic!("Could not open file {:?}: {}", path.display(), err),
+            };
+
+            match file.read_to_string(&mut content) {
+                Ok(_) => {},
+                Err(err) => panic!("Could not read file {:?}: {}", path.display(), err),
+            };
+        }
+        return content;
+    }
+
+    pub fn rewrite_file(path: &Path, new_content: &String) -> Result<(), Error> {
+        let mut file = match File::create(path) {
+            Ok(file) => file,
+            Err(reason) => panic!("Could not open file {:?}: {}", path.display(), reason),
+        };
+
+        return file.write_all(new_content.as_bytes());
+    }
+}
+
 pub mod config {
+    extern crate toml;
+
+    use std::path::Path;
+    use std::io::Error;
+
+
     #[derive(Serialize, Deserialize)]
     pub struct ServerConfig {
         pub data_dir: Option<String>
+    }
+
+    pub fn load_server_config() -> ServerConfig {
+        let file_path = Path::new(super::CONFIG_PATH);
+        let dir_path = file_path.parent().unwrap();
+        super::io::ensure_dir_exists(&dir_path);
+        let actual_content = super::io::get_file_content(&file_path);
+        let server_instance: ServerConfig = toml::from_str(&actual_content).unwrap();
+        return server_instance;
+    }
+
+    pub fn store_server_config(values: &ServerConfig) -> Result<String, Error>  {
+        let file_path = Path::new(super::CONFIG_PATH);
+        let new_config_content = toml::to_string(&values).unwrap();
+        return match super::io::rewrite_file(&file_path, &new_config_content) {
+            Err(e) => Err(e),
+            Ok(_) => Ok(new_config_content),
+        };
     }
 }
 
